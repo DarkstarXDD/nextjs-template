b3471f48bec6a1fa84d45a9b2eba8aec1747100d
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-12-27 12:22:34 +0530

    feat: use perfectionist with eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,38 @@
-import { type Metadata } from "next"
 import { Inter } from "next/font/google"
+import { type Metadata } from "next"
 
 import "@/styles/globals.css"
 
 export const metadata: Metadata = {
-  title: "[Title]",
-  description:
-    "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
-
-  authors: {
-    name: "Darkstar",
-    url: "https://github.com/DarkstarXDD",
-  },
-
   openGraph: {
-    type: "website",
-    url: "https://challenge-name-darkstarxdd.vercel.app/",
-    title: "devfinder",
-
-    description:
-      "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
-
     images: {
       url: "https://challenge-name-darkstarxdd.vercel.app/og-img.jpeg",
       type: "image/jpeg",
       width: 1440,
       height: 756,
     },
+    description:
+      "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
+    url: "https://challenge-name-darkstarxdd.vercel.app/",
+
+    title: "devfinder",
+
+    type: "website",
   },
+  description:
+    "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
+
+  authors: {
+    url: "https://github.com/DarkstarXDD",
+    name: "Darkstar",
+  },
+
+  title: "[Title]",
 }
 
 const inter = Inter({
-  subsets: ["latin"],
   variable: "--font-inter",
+  subsets: ["latin"],
 })
 
 export default function RootLayout({
