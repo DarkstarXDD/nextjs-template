8892f3faadf3ee99a3178fa4a4390b5e3eebec35
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-12-27 12:26:43 +0530

    refactor: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,28 +6,24 @@ import "@/styles/globals.css"
 export const metadata: Metadata = {
   openGraph: {
     images: {
-      url: "https://challenge-name-darkstarxdd.vercel.app/og-img.jpeg",
+      url: "https://project-name-darkstar.vercel.app/og-img.jpeg",
       type: "image/jpeg",
       width: 1440,
-      height: 756,
+      height: 630,
     },
-    description:
-      "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
-    url: "https://challenge-name-darkstarxdd.vercel.app/",
-
-    title: "devfinder",
-
+    url: "https://project-name-darkstar.vercel.app/",
+    description: "Project description",
+    title: "Project Title",
     type: "website",
   },
-  description:
-    "Check out Darkstar's solution for the {challenge name} challenge on Frontend Mentor",
 
   authors: {
     url: "https://github.com/DarkstarXDD",
     name: "Darkstar",
   },
 
-  title: "[Title]",
+  description: "Project description",
+  title: "Project Title",
 }
 
 const inter = Inter({
